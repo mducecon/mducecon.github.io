0cf353818e7389d90c6bf982944f083931319c6e
mducecon <145416269+mducecon@users.noreply.github.com>
2025-07-24 16:42:29 +0000

    Deploying to gh-pages from @ mducecon/mducecon.github.io@07f5ad42aa8683d61ecb4ce334b1478163d95019 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,9 +438,9 @@ ninja.data = [{
           description: "Climate Change Mitigation in Agroforest Landscapes through Sustainable Sourcing Policies \",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-tobacco-retail-price-survey-2025",
+          title: 'Tobacco Retail Price Survey 2025',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
